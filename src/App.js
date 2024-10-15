0beade236477b4db5d32ca5f07526492a1a84abc
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-10-15 20:48:23 -0300

    feat: create form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 const App = () => {
   return (
     <div className="App">
-      <h1>Bem-Vindo ao formulario</h1>
+      <header>
+        <h1>JC form</h1>
+      </header>
+
+      <form className="form">
+        <div>
+          <label>Nome: </label>
+          <input type="name" />
+        </div>
+
+        <div>
+          <label>Email: </label>
+          <input type="email" />
+        </div>
+
+        <div>
+          <label>Aréa de atuação: </label>
+          <input type="checkbox" value="FullStack" />
+          <label>FullStack</label>
+        </div>
+
+        <div>
+          <input type="checkbox" value="Front-End" />
+          <label>Front-End</label>
+        </div>
+
+        <div>
+          <input type="checkbox" value="Back-End" />
+          <label>Back-End</label>
+        </div>
+      </form>
     </div>
   );
 };
